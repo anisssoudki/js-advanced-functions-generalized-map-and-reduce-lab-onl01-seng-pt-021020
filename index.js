43d1749c840906df6bbc5d8240bacf1757b01f0c
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-10-05 01:43:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your functions here
+function map(src, cb) {
+    let result = []
+
+    for (let i = 0; i < src.length; i++) {
+        let element = src[i]
+        result.push(cb(element))
+    }
+
+    return result;
+}
+
+function reduce(src, cb, starting) {
+    let r = (!!starting) ? starting : src[0]
+    let i = (!!starting) ? 0 : 1
+
+    for (; i < src.length; i++) {
+        r = cb(src[i], r)
+    }
+
+    return r;
+}
